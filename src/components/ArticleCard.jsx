75ec122744423f03602029fd13c33c1b7c4fb492
Hamid <alhamid.mustholih97@gmail.com>
2024-03-13 21:17:57 +0700

    Update gambar thumbnail untuk post artikel

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom"
 export default function ArticleCard({ data }) {
     return (
         <div className="card bg-base-100 shadow-xl rounded w-48 md:w-full">
-            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <figure><img className="aspect-video object-cover" src={data._embedded['wp:featuredmedia']['0'].source_url} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{data.title.rendered}</h2>
                 <p className="line-clamp-3" dangerouslySetInnerHTML={{ __html: data.excerpt.rendered }}></p>
                 <div className="card-actions justify-end">
-                    <NavLink to={'/article/' + data.id} className="btn btn-primary">Selengkapnya</NavLink>
+                    <NavLink to={'/article/' + data.id} className="btn btn-sm btn-primary">Selengkapnya</NavLink>
                 </div>
             </div>
         </div>
