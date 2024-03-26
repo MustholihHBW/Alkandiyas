1a689f33604f56f5cf37ba1607795db9d05e334c
Hamid <alhamid.mustholih97@gmail.com>
2024-03-26 19:34:31 +0700

    💄 style: Perbaiki jarak kolom header tabel

--- src/Section/SectionAdminDashboard.jsx
+++ src/Section/SectionAdminDashboard.jsx
@@ -64,7 +64,7 @@ export default function SectionAdminDashboard() {
                     {/* search */}
                     <div className='flex'>
                         <form onSubmit={handleSearch}>
-                            <input onInput={(e) => setSearch(e.target.value)} type="search" placeholder='cari' className='border rounded py-2 px-2 w-24 md:w-48' />
+                            <input onInput={(e) => setSearch(e.target.value)} type="search" placeholder='cari' className='border rounded py-1 px-1 md:py-2 md:px-2 w-24 md:w-48' />
                             <button className='bg-green-500 rounded py-2 px-2 text-white text-sm md:text-base w-10 md:w-20'>
                                 Cari
                             </button>
@@ -72,7 +72,7 @@ export default function SectionAdminDashboard() {
                     </div>
                 </div>
 
-                <div className="container flex flex-col items-center mb-5 text-sm md:text-base">
+                <div className="container flex flex-col items-center gap-10 mb-5 text-sm md:text-base">
                     <table className='w-full flex flex-col gap-3'>
                         {
                             // jika data post lebih dari 0, maka tampilkan data post
@@ -80,17 +80,21 @@ export default function SectionAdminDashboard() {
                                 ? posts.map((item) => {
                                     return (
                                         <tr className='flex justify-between items-center' key={item.id}>
-                                            <td>{item.title.rendered}</td>
+                                            <td>
+                                                <div className='pr-5 md:pr-10'>
+                                                    {item.title.rendered}
+                                                </div>
+                                            </td>
                                             <div className='flex gap-2 md:gap-4'>
                                                 <td>
                                                     <NavLink to={'/admin/edit-post/' + item.id}>
-                                                        <button className='bg-blue-500 rounded p-1 md:p-2 text-sm md:text-base w-14 md:w-20'>
+                                                        <button className='bg-blue-500 rounded p-1 md:p-2 text-sm md:text-base w-14 md:w-20 text-white'>
                                                             Edit
                                                         </button>
                                                     </NavLink>
                                                 </td>
                                                 <td>
-                                                    <button onClick={() => handleDelete(item.id)} className='bg-red-500 rounded p-1 md:p-2 text-sm md:text-base w-14 md:w-20'>
+                                                    <button onClick={() => handleDelete(item.id)} className='bg-red-500 rounded p-1 md:p-2 text-sm md:text-base w-14 md:w-20 text-white'>
                                                         Delete
                                                     </button>
                                                 </td>
